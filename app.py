87e9e67270b8d0183100f66321bf7af986168f9e
hosam <hoadel2003@gmail.com>
2025-02-26 18:01:45 +0200

    .

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
     return jsonify({"detections": detections})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)), debug=True)
